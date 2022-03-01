8eb6bff3329bca8ef9aaa9c24742c94f80d20697
lfeldman <lukasz.feldman@oracle.com>
2022-03-01 16:24:00 +0100

    example wordpress-ha-mds-use-existing-network

--- example/wordpress-ha-mds-use-existing-network/wordpress.tf
+++ example/wordpress-ha-mds-use-existing-network/wordpress.tf
@@ -5,7 +5,7 @@ module "wordpress" {
   source                    = "github.com/oracle-devrel/terraform-oci-arch-wordpress"
   tenancy_ocid              = var.tenancy_ocid
   vcn_id                    = oci_core_virtual_network.wp_mds_vcn.id
-  numberOfNodes             = 1
+  numberOfNodes             = 2
   availability_domain       = var.availability_domain_name == "" ? lookup(data.oci_identity_availability_domains.ADs.availability_domains[0], "name") : var.availability_domain_name
   compartment_ocid          = var.compartment_ocid
   image_id                  = lookup(data.oci_core_images.InstanceImageOCID.images[0], "id")
@@ -14,6 +14,9 @@ module "wordpress" {
   ssh_authorized_keys       = var.ssh_public_key
   mds_ip                    = module.mds-instance.mysql_db_system.ip_address
   wp_subnet_id              = oci_core_subnet.wp_subnet.id
+  lb_subnet_id              = oci_core_subnet.lb_subnet_public.id 
+  bastion_subnet_id         = oci_core_subnet.bastion_subnet_public.id 
+  fss_subnet_id             = oci_core_subnet.fss_subnet_private.id 
   admin_password            = var.admin_password
   admin_username            = var.admin_username
   wp_schema                 = var.wp_schema
@@ -28,5 +31,14 @@ module "wordpress" {
   display_name              = var.wp_instance_name
   flex_shape_ocpus          = var.node_flex_shape_ocpus
   flex_shape_memory         = var.node_flex_shape_memory
+  lb_shape                  = var.lb_shape 
+  flex_lb_min_shape         = var.flex_lb_min_shape 
+  flex_lb_max_shape         = var.flex_lb_max_shape 
+  use_bastion_service       = var.use_bastion_service
+  bastion_image_id          = lookup(data.oci_core_images.InstanceImageOCID2.images[0], "id")
+  bastion_shape             = var.bastion_shape
+  bastion_flex_shape_ocpus  = var.bastion_flex_shape_ocpus
+  bastion_flex_shape_memory = var.bastion_flex_shape_memory
+  bastion_service_region    = var.region 
 }
 
